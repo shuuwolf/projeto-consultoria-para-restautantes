e30efe978c6e21d6d68c5bfc35cc5967647515bf
Rafael Kauch <kauchrafael0001@gmail.com>
2022-07-14 23:20:16 -0300

    section with slider, fix color bg

--- js/app.js
+++ js/app.js
@@ -6,3 +6,12 @@ window.addEventListener("scroll",function(){
 $('#nav-trigger').on('click',() => {
     $('.nav-mobile').slideToggle();
 })
+
+$(".projeto-item").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoPlay: true,
+    speed: 1000,
+    dots: true,
+});
